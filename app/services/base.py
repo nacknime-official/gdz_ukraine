49245519f90d362fc214926d48e28fb90d221947
nacknime-official <lenovo.maks@gmail.com>
2020-07-02 22:54:48 +0300

    refactor: add services/base.py module

---
+++ app/services/base.py
@@ -0,0 +1,34 @@
+"""
+Base logic module
+Not specialize in a specific user
+"""
+
+from aiogram.dispatcher import FSMContext
+
+from app.models.db import db
+
+
+async def set_state_data(state: FSMContext, **state_data) -> None:
+    """
+    Set data into user's state obj
+
+    :param state:           user's state obj
+    :keywords state_data    data that will be set into the state obj
+
+    :returns:               None
+    """
+
+    await state.update_data(state_data)
+
+
+async def set_data_to_db(model: db.Model, **data):
+    """
+    Set data to db through the model
+
+    :param model:       model with which we will set data
+    :keywords data:     data that will be set to db
+
+    :returns:           None
+    """
+
+    await model.update(data).apply()

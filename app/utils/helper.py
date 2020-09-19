9af89e34e28bad06f660605795081bbe5424f858
nacknime-official <lenovo.maks@gmail.com>
2020-09-19 22:09:59 +0300

    Helper: added find_func_by_state_name func

--- app/utils/helper.py
+++ app/utils/helper.py
@@ -1,6 +1,9 @@
-from typing import Optional, Union
+from collections import namedtuple
+from typing import List, Optional, Union
 
 from aiogram.utils.markdown import link
+
+name_func = namedtuple("NameFunction", "name func")
 
 
 def get_user_link(user_id: Union[str, int], text: Optional[str] = None) -> str:
@@ -16,3 +19,22 @@ def get_user_link(user_id: Union[str, int], text: Optional[str] = None) -> str:
     if not text:
         text = user_id
     return link(str(text), f"tg://user?id={user_id}")
+
+
+def find_func_by_state_name(
+    state_name: str, func_list: List[name_func]
+) -> name_func.func:
+    """
+    Find function by state name in list of functions.
+    Used for optimize redis storage - clean unneeded data
+    from the state data.
+
+    :param state_name:  state name
+    :param func_list:   list of functions, in which we'll search
+
+    :returns:           function
+    """
+    parsed_state_name = state_name.split(":")[-1].lower()
+    for name_func in func_list:
+        if name_func.name.startswith(parsed_state_name):
+            return name_func.func

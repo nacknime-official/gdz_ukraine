06c6480ed54e4f026c766f1dbb9518b346eb3c74
nacknime-official <lenovo.maks@gmail.com>
2020-09-27 15:38:17 +0300

    fix(middlewares/checker_input): possible to send 'Назад' when a state is 'Grade'

--- app/middlewares/check_input.py
+++ app/middlewares/check_input.py
@@ -6,7 +6,8 @@ from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram.dispatcher.storage import FSMContext
 
 from app import config
-from app.utils.markups import NAVIGATION_BUTTONS
+from app.utils.helper import find_func_by_state_name
+from app.utils.markups import markups_list
 from app.utils.states import quiz
 
 
@@ -14,8 +15,11 @@ class Checker(BaseMiddleware):
     async def check(self, message: types.Message, state: FSMContext):
         data = await state.get_data()
         current_state = await state.get_state()
-        buttons: List[str] = data.get("Keyboard").get(current_state)
-        buttons += NAVIGATION_BUTTONS
+        buttons_data: List[str] = data.get("Keyboard").get(current_state)
+        keyboard: types.ReplyKeyboardMarkup = find_func_by_state_name(
+            current_state, markups_list
+        )(buttons_data)
+        buttons: List[str] = [j for i in keyboard.keyboard for j in i]
         text = message.text
 
         if text[-1] == "…":
